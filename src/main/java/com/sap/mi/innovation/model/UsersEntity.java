ef763d84c3050c7ca28f18ea19a3897089a02e33
Wu Jason <jason.wu03@sap.com>
2016-01-11 13:39:27 +0800

    jason

---
+++ src/main/java/com/sap/mi/innovation/model/UsersEntity.java
@@ -0,0 +1,93 @@
+package com.sap.mi.innovation.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ * Created by I309891 on 1/6/2016.
+ */
+@Entity
+@Table(name = "users", schema = "public", catalog = "innovation")
+public class UsersEntity{
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String head;
+
+    @Id
+    @Column(name = "email")
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Basic
+    @Column(name = "first_name")
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Basic
+    @Column(name = "last_name")
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Basic
+    @Column(name = "phone")
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Basic
+    @Column(name = "head")
+    public String getHead() {
+        return head;
+    }
+
+    public void setHead(String head) {
+        this.head = head;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UsersEntity that = (UsersEntity) o;
+
+        if (email != null ? !email.equals(that.email) : that.email != null) return false;
+        if (firstName != null ? !firstName.equals(that.firstName) : that.firstName != null) return false;
+        if (lastName != null ? !lastName.equals(that.lastName) : that.lastName != null) return false;
+        if (phone != null ? !phone.equals(that.phone) : that.phone != null) return false;
+        if (head != null ? !head.equals(that.head) : that.head != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = email != null ? email.hashCode() : 0;
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + (phone != null ? phone.hashCode() : 0);
+        result = 31 * result + (head != null ? head.hashCode() : 0);
+        return result;
+    }
+}
