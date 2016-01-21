4b7edb1eed64d9092de9e9b958df24f6ed6ed387
siqi zhuo <siqi.zhuo@qq.com>
2016-01-21 19:41:38 +0800

    add idea_detail page

--- src/main/webapp/www/js/controller/main.js
+++ src/main/webapp/www/js/controller/main.js
@@ -3,104 +3,106 @@ var mainCtcl = angular.module('mainController', []);
 
 
 mainCtcl.controller('homepageCtrl', ['$scope','userMeModel', function($scope,userMeModel){
-	$scope.logInAndOut=function(go,out){
-		if(!$scope.isLogin){
-			go('/user/login', 'slideLeft');
-		}else{
-			out();
+		$scope.logInAndOut=function(go,out){
+			if(!$scope.isLogin){
+				go('/user/login', 'slideLeft');
+			}else{
+				out();
+			}
 		}
-	}
-	$scope.logout=function(){
-		userMeModel.logout();
-		$scope.isLogin=false;
-		$scope.loginOrOut='Login';
-	}
-	$scope.isLogin=userMeModel.isLogin;
-	$scope.loginOrOut=($scope.isLogin?'Logout':'Login')
-}])
-.controller('register1Ctrl', ['$scope', function($scope){
-	//
-}])
-.controller('register2Ctrl', ['$scope', function($scope){
-	//
-}])
-.controller('register3Ctrl', ['$scope','upload' ,'DEFAULT_AVATAR', function($scope,upload,DEFAULT_AVATAR){
-	$scope.imgSrc=DEFAULT_AVATAR;
-	
-	$scope.uploadFile=function(img){
-		var fd = new FormData();
-		fd.append('img',img);
-		
-		upload.upload(fd,function(data){
-			$scope.imgSrc='uploads/1.png';
-		})
-	}
-
-	$scope.clearAndGo=function(fn,a,b){
+		$scope.logout=function(){
+			userMeModel.logout();
+			$scope.isLogin=false;
+			$scope.loginOrOut='Login';
+		}
+		$scope.isLogin=userMeModel.isLogin;
+		$scope.loginOrOut=($scope.isLogin?'Logout':'Login')
+	}])
+	.controller('register1Ctrl', ['$scope', function($scope){
+		//
+	}])
+	.controller('register2Ctrl', ['$scope', function($scope){
+		//
+	}])
+	.controller('register3Ctrl', ['$scope','upload' ,'DEFAULT_AVATAR', function($scope,upload,DEFAULT_AVATAR){
 		$scope.imgSrc=DEFAULT_AVATAR;
-		fn(a,b);
-	}
-}])
-.controller('register4Ctrl', ['$scope', function($scope){
-	$scope.show=false;
-	$scope.save=function(){
-		$scope.show=true;
-	}
-	$scope.fnOk=function(){
-		//console.log(1)
-	}
-	$scope.fnCancel=function(){
+
+		$scope.uploadFile=function(img){
+			var fd = new FormData();
+			fd.append('img',img);
+
+			upload.upload(fd,function(data){
+				$scope.imgSrc='uploads/1.png';
+			})
+		}
+
+		$scope.clearAndGo=function(fn,a,b){
+			$scope.imgSrc=DEFAULT_AVATAR;
+			fn(a,b);
+		}
+	}])
+	.controller('register4Ctrl', ['$scope', function($scope){
 		$scope.show=false;
-	}	
-}])
-.controller('loginCtrl', ['$scope', 'DEFAULT_AVATAR','userMeModel', function($scope,DEFAULT_AVATAR,userMeModel){
-	$scope.imgSrc=DEFAULT_AVATAR;
-	$scope.show=false;
-	$scope.login=function(username,pwd){
-		userMeModel.login(username,pwd);
-		
-		$scope.show=true;
-		//SUCCESS
-		$scope.title="SUCCESS";
-		$scope.content="LOGIN SUCCESS!";
-		$scope.fnOk=function(go){
-			go();
+		$scope.save=function(){
+			$scope.show=true;
 		}
-		//FAILED
-	}
-}])
-.controller('ideaCreateCtrl',['ideaModel','$scope', 'userMeModel', function(ideaM,$scope,userMeModel){
-	userMeModel.needLogin();
-	
-	$scope.idea=ideaM.idea;
-	
-	$scope.remove=function(idx){
-		$scope.idea.imgsSrc.splice(idx,1);
-	}
-	
-	$scope.add=function(file){
-		console.log(file)
-	}	
-	
-	$scope.change=function(e){
-		var file=e.target.files[0];
-		if(!file){
-			return;
+		$scope.fnOk=function(){
+			//console.log(1)
 		}
-		$scope.idea.files.push(file);
-		$scope.idea.imgsSrc.push(URL.createObjectURL(file))
-		$scope.$digest()
-	}
-}])
-.controller('ideaListCtrl',['$scope','ideaListModel',function($scope,list){
-	$scope.ideaList=list.get();
-	$scope.listType=1;
-	
-}])
-.controller('projectListCtrl',['$scope','projectListModel',function($scope,list){
-	$scope.projectList=list.get();
-	$scope.listType=2;
-}])
-.controller('projectCreateCtrl',['$scope','projectCreateModel',function($scope,list){
-	
-}])
+		$scope.fnCancel=function(){
+			$scope.show=false;
+		}
+	}])
+	.controller('loginCtrl', ['$scope', 'DEFAULT_AVATAR','userMeModel', function($scope,DEFAULT_AVATAR,userMeModel){
+		$scope.imgSrc=DEFAULT_AVATAR;
+		$scope.show=false;
+		$scope.login=function(username,pwd){
+			userMeModel.login(username,pwd);
+
+			$scope.show=true;
+			//SUCCESS
+			$scope.title="SUCCESS";
+			$scope.content="LOGIN SUCCESS!";
+			$scope.fnOk=function(go){
+				go();
+			}
+			//FAILED
+		}
+	}])
+	.controller('ideaCreateCtrl',['ideaModel','$scope', 'userMeModel', function(ideaM,$scope,userMeModel){
+		userMeModel.needLogin();
+
+		$scope.idea=ideaM.idea;
+
+		$scope.remove=function(idx){
+			$scope.idea.imgsSrc.splice(idx,1);
+		}
+
+		$scope.add=function(file){
+			console.log(file)
+		}
+
+		$scope.change=function(e){
+			var file=e.target.files[0];
+			if(!file){
+				return;
+			}
+			$scope.idea.files.push(file);
+			$scope.idea.imgsSrc.push(URL.createObjectURL(file))
+			$scope.$digest()
+		}
+	}])
+	.controller('ideaListCtrl',['$scope','ideaListModel',function($scope,list){
+		$scope.ideaList=list.get();
+		$scope.listType=1;
+	}])
+	.controller('projectListCtrl',['$scope','projectListModel',function($scope,list){
+		$scope.projectList=list.get();
+		$scope.listType=2;
+	}])
+	.controller('projectCreateCtrl',['$scope','projectCreateModel',function($scope,list){
+
+	}])
+	.controller('ideaDetailCtrl', ['$scope', 'ideaModel', '$routeParams', function ($scope, idea, $routeParams) {
+
+	}])
--- src/main/webapp/www/js/service/main.js
+++ src/main/webapp/www/js/service/main.js
@@ -7,144 +7,144 @@ service.constant('ANIMAL_IMAGES', [['img/animals/1giraffe_idea.png','img/animals
 	['img/animals/10eagle_idea.png','img/animals/11eagle_designfinish.png','img/animals/12eagle_readyfordone.png']]);
 
 service.factory('upload', ['$http', function($http) {
-	var upload=function(fd,cb){
-		$http.post('/pic',fd, {
-			withCredentials: true,
-			headers: {'Content-Type': undefined },
-			transformRequest: angular.identity
-		})
-		.success(function(data){
-			cb(data)
-		})
-		.error(function(err){
-			console.log(err)
-		})			
-	}
+		var upload=function(fd,cb){
+			$http.post('/pic',fd, {
+					withCredentials: true,
+					headers: {'Content-Type': undefined },
+					transformRequest: angular.identity
+				})
+				.success(function(data){
+					cb(data)
+				})
+				.error(function(err){
+					console.log(err)
+				})
+		}
 
-	return {
-		upload:upload
-	}
-}])
-.factory('ideaModel',[function(){
-	var idea={
-		title:'',
-		description:'',
-		type:'',
-		impact:'',
-		imgsSrc:[],
-		files:[],
-	}
-	var clear=function(){
-		idea.title="";
-		idea.description="";
-		idea.type="";
-		idea.impact="";
-		idea.imgsSrc.length=0;
-		idea.files.length=0;
-	}
-	return{
-		idea:idea,
-		clear:clear
-	}
-}])
-.factory('ideaListModel',[function(){
-	//TEST
-	var temp_item={
+		return {
+			upload:upload
+		}
+	}])
+	.factory('ideaModel',[function(){
+		var idea={
+			title:'',
+			description:'',
+			type:'',
+			impact:'',
+			imgsSrc:[],
+			files:[],
+		}
+		var clear=function(){
+			idea.title="";
+			idea.description="";
+			idea.type="";
+			idea.impact="";
+			idea.imgsSrc.length=0;
+			idea.files.length=0;
+		}
+		return{
+			idea:idea,
+			clear:clear
+		}
+	}])
+	.factory('ideaListModel',[function(){
+		//TEST
+		var temp_item={
 			title:'innovation gomification app',
 			id:66,
 			author:'lili',
 			authorLink:'/user/12',
 			like: 8,
 			description:'fdsafdsfds fda fdsa fdsaf  da f dsa f dsf a dsf ds f dsa f dsa f dsaf ads fds ',
-			src:'https://www.baidu.com/img/bd_logo1.png',	
-			type:'/img/animals/4lion_idea.png',	
+			src:'https://www.baidu.com/img/bd_logo1.png',
+			type:'/img/animals/4lion_idea.png',
 			date:'2016-01-19 16:22',
 			pjState:'1'
 		}
-		
-	var get_list=function(){
-		return [
-			temp_item,
-			angular.copy(temp_item),
-			angular.copy(temp_item),
-			angular.copy(temp_item),
-			angular.copy(temp_item)
-		];
-	}
-	
-	return {
-		get:get_list
-	}
-}])
-.factory('userMeModel', ['$rootScope',function($rootScope){
-	var user={
-		isLogin:false,
-		model:{},
-		login:function(username,pwd){
-			//HTTP POST
-			//SET COOKIE
-			//-----SUCCESS
-			this.isLogin=true;		
-			this.model={};
-		},
-		logout:function(){
-			this.isLogin=false;
-			//HTTP POST 
-			//CLEAR COOKIE
-		},
-		register:function(user){
-			this.isLogin=false;
-			//HTTP POST
-			//----SUCCESS
-			this.isLogin=true;
-			this.model=user;
-		},
-		needLogin:function(){
-			if(!this.isLogin){
-				$rootScope.go('/user/login', 'slideLeft');
-			}
+
+		var get_list=function(){
+			return [
+				temp_item,
+				angular.copy(temp_item),
+				angular.copy(temp_item),
+				angular.copy(temp_item),
+				angular.copy(temp_item)
+			];
 		}
-	};
-	
-	return user;
-}])
-.factory('projectListModel', ['$rootScope',function($rootScope){
-	//TEST
-	var temp_item={
+
+		return {
+			get:get_list
+		}
+	}])
+	.factory('userMeModel', ['$rootScope',function($rootScope){
+		var user={
+			isLogin:false,
+			model:{},
+			login:function(username,pwd){
+				//HTTP POST
+				//SET COOKIE
+				//-----SUCCESS
+				this.isLogin=true;
+				this.model={};
+			},
+			logout:function(){
+				this.isLogin=false;
+				//HTTP POST
+				//CLEAR COOKIE
+			},
+			register:function(user){
+				this.isLogin=false;
+				//HTTP POST
+				//----SUCCESS
+				this.isLogin=true;
+				this.model=user;
+			},
+			needLogin:function(){
+				if(!this.isLogin){
+					$rootScope.go('/user/login', 'slideLeft');
+				}
+			}
+		};
+
+		return user;
+	}])
+	.factory('projectListModel', ['$rootScope',function($rootScope){
+		//TEST
+		var temp_item={
 			title:'innovation gomification app',
 			id:66,
 			author:'lili',
 			authorLink:'/user/12',
 			like: 8111,
 			description:'fdsafdsfds fda fdsa fdsaf  da f dsa f dsf a dsf ds f dsa f dsa f dsaf ads fds ',
-			src:'https://www.baidu.com/img/bd_logo1.png',	
-			type:'/img/animals/4lion_idea.png',	
+			src:'https://www.baidu.com/img/bd_logo1.png',
+			type:'/img/animals/4lion_idea.png',
 			date:'2016-01-19 16:22',
 			pjState:'4'
 		}
-		
-	var get_list=function(){
-		return [
-			temp_item,
-			angular.copy(temp_item),
-			angular.copy(temp_item),
-			angular.copy(temp_item),
-			angular.copy(temp_item)
-		];
-	}
-	
-	return {
-		get:get_list
-	}
-}])
-.factory('projectCreateModel',[function(){
-	var project={
-	}
-	var clear=function(){
 
-	}
-	return{
-		project:project,
-		clear:clear
-	}
-}])
+		var get_list=function(){
+			return [
+				temp_item,
+				angular.copy(temp_item),
+				angular.copy(temp_item),
+				angular.copy(temp_item),
+				angular.copy(temp_item)
+			];
+		}
+
+		return {
+			get:get_list
+		}
+	}])
+	.factory('projectCreateModel',[function(){
+		var project={
+		}
+		var clear=function(){
+
+		}
+		return{
+			project:project,
+			clear:clear
+		}
+	}])
