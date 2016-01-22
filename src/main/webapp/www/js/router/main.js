eb0e94357a790e8e5d2702bb5b303191a042c780
unknown <I327069@PVGN50842090A.apj.global.corp.sap>
2016-01-22 18:19:28 +0800

    PJ_ADD_PEOPLE

--- src/main/webapp/www/js/router/main.js
+++ src/main/webapp/www/js/router/main.js
@@ -24,75 +24,90 @@ router.config(['$routeProvider', function($routeProvider){
             controller : 'register4Ctrl',
             templateUrl : 'template/user/register_step4.html'
         })
-        .when('/user/login', {
+		.when('/user/login', {
             controller : 'loginCtrl',
-            templateUrl : 'template/user/login.html'
-        })
-        .when('/idea/create', {
+            templateUrl : 'template/user/login.html'			
+		})
+		.when('/user/list', {
+            controller : 'userListCtrl',
+            templateUrl : 'template/user/list.html'			
+		})	
+		.when('/user/add_list', {
+            controller : 'userAddProjectCtrl',
+            templateUrl : 'template/user/add_list.html'			
+		})			
+		.when('/idea/create', {
             controller : 'ideaCreateCtrl',
-            templateUrl : 'template/idea/create.html'
-        })
-        .when('/idea/create_step1', {
+            templateUrl : 'template/idea/create.html'			
+		})
+		.when('/idea/create_step1', {
             controller : 'ideaCreateCtrl',
-            templateUrl : 'template/idea/create_step1.html'
-        })
-        .when('/idea/create_step2', {
+            templateUrl : 'template/idea/create_step1.html'			
+		})
+		.when('/idea/create_step2', {
             controller : 'ideaCreateCtrl',
-            templateUrl : 'template/idea/create_step2.html'
-        })
-        .when('/idea/create_step3', {
+            templateUrl : 'template/idea/create_step2.html'			
+		})
+		.when('/idea/create_step3', {
             controller : 'ideaCreateCtrl',
-            templateUrl : 'template/idea/create_step3.html'
-        })
-        .when('/idea/create_step4', {
+            templateUrl : 'template/idea/create_step3.html'			
+		})
+		.when('/idea/create_step4', {
             controller : 'ideaCreateCtrl',
-            templateUrl : 'template/idea/create_step4.html'
-        })
-        .when('/idea/idea_preview', {
+            templateUrl : 'template/idea/create_step4.html'			
+		})
+		.when('/idea/idea_preview', {
             controller : 'ideaCreateCtrl',
             templateUrl : 'template/idea/idea_preview.html'
         })
-        .when('/idea/list', {
+		.when('/idea/list', {
             controller : 'ideaListCtrl',
-            templateUrl : 'template/idea/list.html'
-        })
-        .when('/project/list', {
-            controller : 'projectListCtrl',
-            templateUrl : 'template/project/list.html'
-        })
-        .when('/idea/:id', {
+            templateUrl : 'template/idea/list.html'			
+		})
+		.when('/idea/:id', {
             controller : 'ideaDetailCtrl',
             templateUrl : 'template/idea/idea_detail.html'
-        })
-        .when('/project/create_step1', {
+        })			
+		.when('/project/list', {
+            controller : 'projectListCtrl',
+            templateUrl : 'template/project/list.html'			
+		})	
+		.when('/project/create_step1', {
             controller : 'projectCreateCtrl',
-            templateUrl : 'template/project/list.html'
-        })
-        .when('/project/create_step2', {
+            templateUrl : 'template/project/create_step1.html'			
+		})
+		.when('/project/create_step2', {
             controller : 'projectCreateCtrl',
-            templateUrl : 'template/project/list.html'
-        })
-        .when('/project/create_step3', {
+            templateUrl : 'template/project/create_step2.html'			
+		})
+		.when('/project/create_step3', {
             controller : 'projectCreateCtrl',
-            templateUrl : 'template/project/list.html'
-        })
-        .when('/project/create_step4', {
+            templateUrl : 'template/project/create_step3.html'			
+		})
+		.when('/project/create_step4', {
             controller : 'projectCreateCtrl',
-            templateUrl : 'template/project/list.html'
-        })
-        .when('/project/create_step5', {
+            templateUrl : 'template/project/create_step4.html'			
+		})
+		.when('/project/create_step5', {
             controller : 'projectCreateCtrl',
-            templateUrl : 'template/project/list.html'
-        })
-        .when('/project/create_step6', {
+            templateUrl : 'template/project/create_step5.html'			
+		})
+		.when('/project/create_step6', {
             controller : 'projectCreateCtrl',
-            templateUrl : 'template/project/list.html'
-        })
+            templateUrl : 'template/project/create_step6.html'			
+		})
+		.when('/project/create_finish', {
+            controller : 'projectCreateCtrl',
+            templateUrl : 'template/project/create_finish.html'			
+		})
         .when('/project/project_preview', {
             controller : 'projectCreateCtrl',
             templateUrl : 'template/project/project_preview.html'
-        })
+        })		
+		.when('/404', {
+            templateUrl : 'template/404.html'			
+		})		
         .otherwise({
-            redirectTo : 'homepage'
+            redirectTo : '404'
         });
 }]);
